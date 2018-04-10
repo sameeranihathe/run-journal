695eb5c01be91b13a99efaed11757af835be4d43
sameeranihathe <sameeranihathe@gmail.com>
2018-04-10 23:09:14 +0530

    components added and gpx files added

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -5,7 +5,7 @@ export const SAVED_ACTIVITIES: IActivity[] =[
         "id" : 1,
         "name" : "Main Bike Trails",
         "date" : new Date('12/04/2018'),
-        "distance" : "5.3",
+        "distance" : 5.3,
         "gpxData" : '../../assets/gpx/1.gpx'
         
     }
