6d12b835645619792949312b74f89c9c733a0d62
sameeranihathe <sameeranihathe@gmail.com>
2018-04-11 08:36:10 +0530

    activity service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ActivityListComponent } from './activity-list/activity-list.component';
 import { MapComponent } from './map/map.component';
-
+import { ActivityService} from './services/activity.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { MapComponent } from './map/map.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ActivityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -3,10 +3,36 @@ import { IActivity} from './activity.model';
 export const SAVED_ACTIVITIES: IActivity[] =[
     {
         "id" : 1,
-        "name" : "Main Bike Trails",
-        "date" : new Date('12/04/2018'),
-        "distance" : 5.3,
-        "gpxData" : '../../assets/gpx/1.gpx'
+  "name" : "Main Bike Trails",
+  "date" : new Date('06/01/2017'),
+  "distance" : 16.2,
+  "comments" : "Nice day, cool temps",
+  "gpxData": '../../assets/gpx/1.gpx'
+},
+{
+   "id": 2,
+   "name": 'Industrial Park',
+   "date": new Date('06/04/2017'),
+   "gpxData": '../../assets/gpx/1.gpx',
+   "distance": 1.2,
+   "comments": 'Cool and windy.'
+ },
+ {
+   "id": 3,
+   "name": 'Forest Route',
+   "date": new Date('06/05/2017'),
+   "gpxData": '../../assets/gpx/2.gpx',
+   "distance": 3.2,
+   "comments": 'Evening run.'
+ },
+ {
+   "id": 4,
+   "name": 'Lake Shore',
+   "date": new Date('06/08/2017'),
+   "gpxData": '../../assets/gpx/4.gpx',
+   "distance": 8.4,
+   "comments": 'Cool and windy by the lake.'
+ }
         
-    }
+    
 ]
